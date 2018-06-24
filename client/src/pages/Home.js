3fc62d7b4b3e287452fca70511c24df20d11e493
Elisa Urda <urda.elisa@gmail.com>
2018-06-24 19:38:56 -0400

    started working on dynamic home component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-const Home = () => (
-    <div>
-        <h1>Welcome to Buttered Popcorn!</h1>
-        <a href="/register"><h3>Create an Account</h3></a>
-        <a href="/login"><h3>Log In</h3></a>
-    </div>
-)
+class Home extends Component {
+    state = {
+        user: null
+    }
+
+    
+
+    render() {
+        (
+            <div>
+                <h1>Welcome to Buttered Popcorn!</h1>
+                {this.state.user ? (
+                    <div>
+                        <h3>You are currently logged in as{this.state.user.username}</h3>
+                    </div>
+                ) : (
+                        <div>
+                            <a href="/register"><h3>Create an Account</h3></a>
+                            <a href="/login"><h3>Log In</h3></a>
+                        </div>)
+                }
+            </div>
+        )
+    }
+}
 
 export default Home
