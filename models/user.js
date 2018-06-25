02a6f53750b522ed173a11d46eef1916fcf62503
Elisa Urda <urda.elisa@gmail.com>
2018-06-25 14:49:30 -0400

    created user model

---
+++ models/user.js
@@ -0,0 +1,29 @@
+module.exports = function (sequelize, DataTypes) {
+
+    var User = sequelize.define("User", {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: 2,
+                    msg: "Name must be at least two characters in length"
+                }
+            }
+        },
+
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+    return User;
+};
