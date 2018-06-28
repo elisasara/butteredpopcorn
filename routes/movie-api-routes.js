612f85da54f42484d9ab9cf68ae2062aa7cef2ab
Elisa Urda <urda.elisa@gmail.com>
2018-06-27 20:17:08 -0400

    api call to tmdb still not working

--- routes/movie-api-routes.js
+++ routes/movie-api-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require("axios");
-const CircularJSON = require("circular-json");
+// const CircularJSON = require("circular-json");
 require("dotenv").config();
 
 const APIKey = process.env.TMDB_API_Key
