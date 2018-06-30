c857dde1fdfedfe59f0cd3ee79fc9bdaec229eca
Elisa Urda <urda.elisa@gmail.com>
2018-06-30 13:58:45 -0400

    commented to do items

--- client/src/components/InfoPage.js
+++ client/src/components/InfoPage.js
@@ -21,6 +21,9 @@ class InfoPage extends Component {
     }
 
     render() {
+        // put in db entering info here as well. float left and right for that info
+        // work on formating of movie page
+        // work on tv version of this as well
         return (
             <div>
                 <MovieInfo info={this.state.info} />
