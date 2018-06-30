b599486615fce63298e1a5de5b73d5ee4390929d
Elisa Urda <urda.elisa@gmail.com>
2018-06-30 17:31:10 -0400

    working on database logic

--- client/src/components/DBInfo.js
+++ client/src/components/DBInfo.js
@@ -11,16 +11,22 @@ const DBInfo = props => {
     };
 
     return (
-        <div className="col-md-4">
-            <h3>Mark as:</h3>
-            <div className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropbown Button</button>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a className="dropdown-item" href="#">Want To Watch</a>
-                    <a className="dropdown-item" href="#">Watching</a>
-                    <a className="dropdown-item" href="#">Watched</a>
-                </div>
-            </div>
+        // <div className="col-md-4">
+        //     <h3>Mark as:</h3>
+        //     <div className="dropdown">
+        //         <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropbown Button</button>
+        //         <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        //             <a className="dropdown-item" href="#">Want To Watch</a>
+        //             <a className="dropdown-item" href="#">Watching</a>
+        //             <a className="dropdown-item" href="#">Watched</a>
+        //         </div>
+        //     </div>
+        // </div>
+
+
+        // THIS HAS NOT BEEN TESTED YET!!!
+        <div className="container">
+            <button onclick={()=> this.wantToWatch(props.info.id, props.user.id)} className="btn" type="button">Want to Watch</button>
         </div>
     )
 }
