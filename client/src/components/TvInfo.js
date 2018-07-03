c9ce0ca0519ff15a9d9531d6c4dddf8a5f3a9f75
Elisa Urda <urda.elisa@gmail.com>
2018-07-03 16:59:04 -0400

    tvinfo component working

--- client/src/components/TvInfo.js
+++ client/src/components/TvInfo.js
@@ -1,18 +1,29 @@
 import React from "react";
 
 const TvInfo = props => {
-    const id=props.info.id;
+    const id = props.info.id;
     console.log("id: ", id);
-    const title=props.info.name;
+    const title = props.info.name;
     console.log("title: ", title);
-    const seasons=props.info.number_of_seasons;
+    const seasons = props.info.number_of_seasons;
     console.log("seasons: ", seasons);
-    const overview=props.info.overview;
+    const overview = props.info.overview;
     console.log("overview: ", overview);
-    const image=`https://image.tmdb.org/t/p/w300/${props.info.poster_path}`;
-    console.log("image: ", image);
+    const imageURL = `https://image.tmdb.org/t/p/w300/${props.info.poster_path}`;
+    console.log("imageURL: ", imageURL);
     return (<div>
-        <h1>TV Info here</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-4">
+                    <img src={imageURL} alt="poster" />
+                </div>
+                <div className="col-md-4">
+                    <h3>{title}</h3>
+                    <p className="font-italic">Seasons: {seasons}</p>
+                    <p>{overview}</p>
+                </div>
+            </div>
+        </div>
     </div>)
 }
 
