e93de8ee25fd385eba74e7910616f0072570ddba
Elisa Urda <urda.elisa@gmail.com>
2018-07-11 18:01:07 -0400

    one small change. still working on newsfeed

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -14,28 +14,30 @@ class Feed extends Component {
         dbAPI.getNewsfeed()
             .then(res => {
                 console.log(res.data);
+                
                 this.setState({
-                    feed: []
+                    feed: 
+                    })
                 })
                 // for each record go through and do a for each for currently watching/watching/watched and add to one array
                 // sort array in order of updated at date
                 // set state to that array
-            //     let feedArr = [];
-            //     res.data.forEach(function(record) {
-            //         record.currentlyWatchings.forEach(function(item1){
-            //             feedArr.push(item1);
-            //         });
-            //         record.Watchings.forEach(function(item2){
-            //             feedArr.push(item2);
-            //         });
-            //         record.Watcheds.forEach(function(item3){
-            //             feedArr.push(item3);
-            //         });
-            //     })
-            //     this.setState({
-            //         feed: feedArr
-            //     });
-            //     console.log("feed state: ", this.state.feed)
+                //     let feedArr = [];
+                //     res.data.forEach(function(record) {
+                //         record.currentlyWatchings.forEach(function(item1){
+                //             feedArr.push(item1);
+                //         });
+                //         record.Watchings.forEach(function(item2){
+                //             feedArr.push(item2);
+                //         });
+                //         record.Watcheds.forEach(function(item3){
+                //             feedArr.push(item3);
+                //         });
+                //     })
+                //     this.setState({
+                //         feed: feedArr
+                //     });
+                //     console.log("feed state: ", this.state.feed)
             });
     };
 
@@ -45,8 +47,8 @@ class Feed extends Component {
                 {this.state.feed.length ? (
                     <h1>This is where your newsfeed will go</h1>
                 ) : (
-                    <h1>You don't have any friend activity. Click here to find your friends.</h1>
-                )}
+                        <h1>You don't have any friend activity. Click here to find your friends.</h1>
+                    )}
             </div>
         );
     };
