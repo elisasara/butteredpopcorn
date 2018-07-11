d8efb4cd628eef5198ede5062e034ac2d32b25f6
Elisa Urda <urda.elisa@gmail.com>
2018-07-11 17:32:44 -0400

    trying to figure out array logic for getting all items for newsfeed

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -14,9 +14,25 @@ class Feed extends Component {
         dbAPI.getNewsfeed()
             .then(res => {
                 console.log(res.data);
+                // for each record go through and do a for each for currently watching/watching/watched and add to one array
+                // sort array in order of updated at date
+                // set state to that array
+                let feedArr = [];
+                res.data.forEach(function(record) {
+                    record.currentlyWatchings.forEach(function(item1){
+                        feedArr.push(item);
+                    });
+                    record.Watchings.forEach(function(item2){
+                        feedArr.push(item);
+                    });
+                    record.Watcheds.forEach(function(item3){
+                        feedArr.push(item);
+                    });
+                })
                 this.setState({
-                    feed: res.data
+                    feed: feedArr
                 });
+                console.log("feed state: ", this.state.feed)
             });
     };
 
