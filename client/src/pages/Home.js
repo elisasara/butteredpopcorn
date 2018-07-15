3daea54a02184c29bd055559a9c542ef197ae574
Elisa Urda <urda.elisa@gmail.com>
2018-07-15 12:33:02 -0400

    more homepage styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Home = () => (
-    <div>
-        <h1>Welcome to Buttered Popcorn!</h1>
+    <div className="text-center" id="home">
+        <h1 className="display-3">Welcome to<br />Buttered Popcorn!</h1>
+        <h3><Link to="/register" style={{color: "#ef8b8b"}}>Register here</Link> to get started!</h3>
+        <h5>Already have an account? <Link to="/login" style={{color: "#ef8b8b"}}>Login here</Link>.</h5>
     </div>
 )
 
