de937878cca003cf48f0f29ad445583cf401d136
Elisa Urda <urda.elisa@gmail.com>
2018-07-15 15:37:06 -0400

    added default image if no image available.

--- client/src/components/MovieInfo.js
+++ client/src/components/MovieInfo.js
@@ -1,18 +1,29 @@
 import React from "react";
 
 const MovieInfo = props => {
-    // make smart component so that it will know how to handle no poster
-    
+
     const name = props.info.title;
+    const imagePath = props.info.poster_path;
     const imageURL = `https://image.tmdb.org/t/p/w200/${props.info.poster_path}`;
     const overview = props.info.overview;
     const releaseDate = props.info.release_date;
     return (
         <div>
-            <img className="img-fluid img-thumbnail" id="moviePoster" src={imageURL} alt="poster" />
-            <h3>{name}</h3>
-            <p className="font-italic movieInfo">{releaseDate}</p>
-            <p className="movieInfo">{overview}</p>
+            {imagePath ? (
+                <div>
+                    <img className="img-fluid img-thumbnail moviePoster" src={imageURL} alt="poster" />
+                    <h3>{name}</h3>
+                    <p className="font-italic movieInfo">{releaseDate}</p>
+                    <p className="movieInfo">{overview}</p>
+                </div>
+            ) : (
+                <div>
+                <img className="img-fluid img-thumbnail moviePoster" src="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg" alt="poster" />
+                <h3>{name}</h3>
+                <p className="font-italic movieInfo">{releaseDate}</p>
+                <p className="movieInfo">{overview}</p> 
+                </div>
+            )}
         </div>
     )
 };
