3191d22be838f5c3e13a96c2c860ff941588b536
Elisa Urda <urda.elisa@gmail.com>
2018-07-19 15:21:32 -0400

    final touches for demo presentation

--- client/src/components/Friend.js
+++ client/src/components/Friend.js
@@ -1,11 +1,12 @@
 import React from "react";
 import AddFriendBtn from "./AddFriendBtn";
+import moment from "moment";
 
 const Friend = props => (
     <div className="row indFriend">
         <div className="col md-8">
             <h3>{props.name}</h3>
-            <p>Joined Buttered Popcorn: {props.joined}</p>
+            <p>Joined Buttered Popcorn: {moment(props.joined).format("MMMM DD, YYYY")}</p>
         </div>
         <div className="col md-4">
             <center><AddFriendBtn friendId={props.friendId} /></center>
