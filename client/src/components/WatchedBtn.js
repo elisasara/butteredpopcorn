b41bb8fc45179fd9086d969efe1781a2bac1fe53
Elisa Urda <urda.elisa@gmail.com>
2018-07-19 16:47:20 -0400

    fixing small bug for presentation

--- client/src/components/WatchedBtn.js
+++ client/src/components/WatchedBtn.js
@@ -6,8 +6,8 @@ const WatchedBtn = props => (
         {props.rating ? (
             <div>
                 <div className={props.btnstyle} {...props}>Watched</div>
-                {/* <p>You rated this: {props.rating}</p> */}
-                <p>You rated this:<span><Stars stars={props.rating} /></span></p>
+                <p>You rated this: {props.rating}</p>
+                {/* <p>You rated this:<span><Stars stars={props.rating} /></span></p> */}
             </div>
         ) : (
                 <div className={props.btnstyle} {...props}>Watched</div>
