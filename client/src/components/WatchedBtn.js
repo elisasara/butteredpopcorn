95848f1089658fcdd6fb6e186c0ada5cf0552d79
Elisa Urda <urda.elisa@gmail.com>
2018-08-16 20:23:23 -0400

    rating showing up correctly now

--- client/src/components/WatchedBtn.js
+++ client/src/components/WatchedBtn.js
@@ -3,15 +3,15 @@ import Stars from "./Stars";
 
 const WatchedBtn = props => (
     <div>
-        {/* {props.rating ? ( */}
+        {props.rating ? (
             <div>
                 <div className={props.btnstyle} {...props}>Watched</div>
                 {/* <p>You rated this: {props.rating}</p> */}
-                {/* <p>You rated this:<span><Stars stars={props.rating} /></span></p> */}
+                <p>You rated this:<span><Stars stars={props.rating} /></span></p>
             </div>
-        {/* ) : ( */}
-                {/* <div className={props.btnstyle} {...props}>Watched</div>
-            )} */}
+        ) : (
+                <div className={props.btnstyle} {...props}>Watched</div>
+            )}
     </div>
 )
 
